bf4945b0f8868594cf38b563fd1572f976228385
Lucas <thegreatberenger@gmail.com>
2024-02-16 20:19:09 -0300

    add more try-except and while loops for the correct functionalitie of the aplication

---
+++ bank/README.md
@@ -0,0 +1,17 @@
+# Simple Banking Aplication
+
+This is the final project for the Python module in the course "The Ultimate 2023 Fullstack Web Development Bootcamp."
+I highly recommend this course if you want to learn how to code. Check it out: https://www.udemy.com/course/the-ultimate-fullstack-web-development-bootcamp/
+
+## How to use the Bank?
+
+You only will need a text editor to run The Banking Aplication.
+
+1. Open bank.py in your terminal (or cmd, or bash, or whatever the kind of terminal you like);
+2. Run 'python bank.py' (Make sure you are in the correct directory);
+3. Choose between the options: you can check the balance (1), deposit (2) and withdraw (3). The fourth (4) option closes the app;
+4. Everytime you choose to deposit or withdraw, once the operation has been completed, the aplication will generate a receipt.
+
+---
+
+📝 Documentation by Lucas Berenger | 📆 2024 |
--- bank/bank.py
+++ bank/bank.py
@@ -1,11 +1,15 @@
 class Bank:
     bank_name = "Itau"
     user_name = "uknown" 
+    deposit_receipt_counter = 0
+    withdraw_receipt_counter = 0
     
     def __init__(self, user_name):
         self.user_name = user_name
         self.balance = 1000
-        print("welcome", self.user_name)
+        print("-----------------------")
+        print("Welcome", self.user_name)
+        print("-----------------------")
 
     
     def start(self):
@@ -37,42 +41,73 @@ class Bank:
         self.start()
     
     def deposit(self):
-        deposit_value = input("How much do you wish to deposit?")
-        confirm = input(f"Please, confirm your deposite: \n value:{deposit_value} \n Yes (y) or No (n)?")
-        self.balance = self.balance + int(deposit_value)
+        while True:
+            self.deposit_receipt_counter += 1
+            deposit_value = input("How much do you wish to deposit?")
 
-        if confirm == "y":
-            print("Success!")
-            print("Printing deposit receipt...")
-            with open('deposit_receipt.txt', 'w') as file:
-                file.write(f"deposit value: {deposit_value}\n updated balance: {self.balance} ")
-            with open('deposit_receipt.txt', 'r') as file:
-                print(file.read())
-            self.start()
-        else:
-            self.deposit()
+            try:
+                self.balance = self.balance + int(deposit_value)
+            except ValueError as e:
+                print(f"{e}: Only numbers are allowed.")
+                continue  
+
+            confirm = input(f"Please, confirm your deposite: \n value:{deposit_value} \n Yes(y) or No(n)?")
+
+            try:
+                if confirm == "y":
+                    print("Success!")
+                    print("Printing deposit receipt...")
+                    with open(f'deposit_receipt_{self.deposit_receipt_counter}.txt', 'w') as file:
+                        file.write(f"deposit value: R${deposit_value}\nupdated balance: R${self.balance} ")
+                    with open(f'deposit_receipt_{self.deposit_receipt_counter}.txt', 'r') as file:
+                        print(file.read())
+                    self.start()
+                    return
+                elif confirm == "n":
+                    continue
+                else:
+                    raise ValueError("Invalid Option")
+            except ValueError as e:
+                print(f"Error: {e}. Please, select a valid option.")
+                continue
     
     def withdraw(self):
-        withdraw_value = input("How much do you wish to withdraw?")
-        confirm = input(f"Please, confirm your withdraw:\n value:{withdraw_value} \n Yes (y) or No (n)?")
-        self.balance = self.balance - int(withdraw_value)
-        
-        if confirm == "y":
-            print("Success!")
-            print("Printing withdraw receipt...")
-            with open('withdraw_receipt.txt', 'w') as file:
-                file.write(f"withdraw value: {withdraw_value} \n updated balance: {self.balance}")
-            with open('withdraw_receipt.txt', 'r') as file:
-                print(file.read())
-                self.start()
-        else: 
-            self.withdraw()
+        while True:
+            self.withdraw_receipt_counter += 1
+            withdraw_value = input("How much do you wish to withdraw?")
+            
+            try:
+                self.balance = self.balance - int(withdraw_value)
+            except ValueError as e:
+                print(f"{e}: Only numbers are allowed.")
+                continue  
+            
+            confirm = input(f"Please, confirm your withdraw:\n value:{withdraw_value} \n Yes(y) or No(n)?")
+            
+            try:
+                if confirm == "y":
+                    print("Success!")
+                    print("Printing withdraw receipt...")
+                    with open(f'withdraw_receipt_{self.withdraw_receipt_counter}.txt', 'w') as file:
+                        file.write(f"withdraw value: R${withdraw_value}\nupdated balance: R${self.balance}")
+                    with open(f'withdraw_receipt_{self.withdraw_receipt_counter}.txt', 'r') as file:
+                        print(file.read())
+                        self.start()
+                        return
+                elif confirm == "n": 
+                    continue
+                else:
+                    raise ValueError("Invalid Option")
+            except ValueError as e:
+                print(f"Error: {e}. Please, select a valid option.")
+                continue
+    
     
     def exit(self):
         return True
             
    
-login = Bank("Lucas Berenger")
+login = Bank("username")
 
 
 while True:
